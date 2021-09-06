f21de0e3bfacb0314836edc906b8ae8c3947f851
Danster4 <86613317+Danster4@users.noreply.github.com>
2021-09-06 11:41:31 -0400

    add strings instead of functions for character/variable list, add options for each choice made, For loop to generate variable and through into array, and then thrown into string

--- Develop/script.js
+++ Develop/script.js
@@ -1,82 +1,107 @@
 // Assignment code here
+var designateLength;
+var includeCapitalLetters;
+var includeLowerCaseLetters;
+var includeNumbers;
+var includeSymbols;
+var choices;
 
+// Array for characters
+var capitalLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+var symbols = ["!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "", "]", "^", "`", "{", "|", "}", "~" ];
 
-
-// const includeLowerCaseLetters = document.getElementById("lowerCaseLetters");
-// const includeNumbers = document.getElementById ("numbers");
-// const includeSymbols = document.getElementById("symbols")
-
-// var makePassword = function () {
-//   var characterLength = prompt("How many characters would you like your password to be?")
-//   var confirmCapitalLetters = confirm("Would you like Capital Letters (i.e. A, B, C) in your password? Select Okay for Yes");
-//     if (confirmCapitalLetters) = getElementById
-//   var confirmLowerCaseLetters = confirm("Would you like Lower Case Letters (i.e. a, b, c) in your password? Select Okay for Yes");
-//   var confirmNumbersLetters = confirm("Would you like Numbers (i.e. 1, 2, 3)in your password? Select Okay for Yes");
-//   var confirmSpecialCharacters = confirm("Would you like Special Characters (ex. !@#$%^&*) in your password? Select Okay for Yes");
-// }
-
-
-
-
-
-const generatePassword = function() {
+// function to generate password when button clicked
+var generatePassword = function() {
   alert("Hello! Welcome to Password Generator!" +"\n" + "Please answer the following prompts and questions! Thank you!")
   
-  const designateLength = prompt("How many characters would you like your password to be?"); {
+    designateLength = parseInt(prompt("How many characters would you like your password to be?")); {
+    if (!designateLength) {
+      alert("Please enter a password length value.");
+    }
+    else { 
+      // Confirm different variables (characters) to include in password
+      includeCapitalLetters = confirm("Would you like Capital Letters (i.e. A, B, C) in your password?" +"\n" + "Select Okay to include Capital Letters.");
+      includeLowerCaseLetters = confirm("Would you like Lower Case Letters (i.e. a, b, c) in your password?" + "\n" + "Select Okay to include Lower Case Letters.");
+      includeNumbers = confirm("Would you like Numbers (i.e. 1, 2, 3) in your password?" + "\n" + "Select Okay to include Numbers.");
+      includeSymbols = confirm("Would you like Symbols (i.e. !$%&'()*+,-./:;<=>?@[^/]_`{|}~) in your password?" + "\n" + "Select Okay to include Symbols.");
+    };
     
+    // If all 4 variables (confirms) are chosen
+    if (includeCapitalLetters && includeLowerCaseLetters && includeNumbers && includeSymbols) {
+      var choices = capitalLetters.concat(lowerCaseLetters, numbers, symbols)
+    }
+
+    // Else if for 4 negative options (none chosen)
+    else if (!includeCapitalLetters && !includeLowerCaseLetters && !includeNumbers && !includeSymbols) {
+      choices = alert("Please choose at least one criteria!")
+    }
+
+    // Else if options for 3 choices
+    else if (includeCapitalLetters && includeLowerCaseLetters && includeNumbers) {
+      var choices = capitalLetters.concat(lowerCaseLetters, numbers)
+    }
+    else if (includeCapitalLetters && includeLowerCaseLetters && includeSymbols) {
+      var choices = capitalLetters.concat(lowerCaseLetters, symbols)
+    }
+    else if (includeCapitalLetters && includeSymbols && includeNumbers) {
+      var choices = capitalLetters.concat(symbols, numbers)
+    }
+    else if (includeSymbols && includeLowerCaseLetters && includeNumbers) {
+      var choices = symbols.concat(lowerCaseLetters, numbers)
+    }
+
+    // Else if options for 2 choices
+    else if (includeCapitalLetters && includeLowerCaseLetters) {
+      var choices = capitalLetters.concat(lowerCaseLetters)
+    }
+    else if (includeCapitalLetters && includeNumbers) {
+      var choices = capitalLetters.concat(numbers)
+    }
+    else if (includeCapitalLetters && includeSymbols) {
+      var choices = capitalLetters.concat(symbols)
+    }
+    else if (includeLowerCaseLetters && includeNumbers) {
+      var choices = lowerCaseLetters.concat(numbers)
+    }
+    else if (includeLowerCaseLetters && includeSymbols) {
+      var choices = lowerCaseLetters.concat(symbols)
+    }
+    else if (includeNumbers && includeSymbols) {
+      var choices = numbers.concat(symbols)
+    }
+
+    //Else if options for 1 choice
+    else if (includeCapitalLetters) {
+      var choices = capitalLetters
+    }
+    else if (includeLowerCaseLetters) {
+      var choices = lowerCaseLetters
+    }
+    else if (includeNumbers) {
+      var choices = numbers
+    }
+    else if (includeSymbols) {
+      var choices = symbols
+    }
+
+    // Empty array to throw output on final choices into
+    var passwordArray = [];
+
+    // For loop to randomly generate from the choices variable until the length designated is reached. Output it to passwordArray
+    for (var i = 0; i < designateLength; i++) {
+      var finalChoices = choices[Math.floor(Math.random() * choices.length)];
+      passwordArray.push(finalChoices)
+    }
+
+    // Transform the array of passwordArray into a string called passwordString, as my final output for the function generatePassword()
+    var passwordString = passwordArray.join("");
+    console.log(passwordString)
+    return passwordString;
   }
   
-  const includeCapitalLetters = confirm("Would you like Capital Letters (i.e. A, B, C) in your password?" +"\n" + "Select Okay to include Capital Letters.");
-    if (includeCapitalLetters == true) {
-      randomFunction.capital
-    }
-    
-  const includeLowerCaseLetters = confirm("Would you like Lower Case Letters (i.e. a, b, c) in your password?" + "\n" + "Select Okay to include Lower Case Letters.");
-    if (includeLowerCaseLetters == true) {
-      randomFunction.lower
-    }
-
-  const includeNumbers = confirm("Would you like Numbers (i.e. 1, 2, 3) in your password?" + "\n" + "Select Okay to include Numbers.");
-    if (includeNumbers == true) {
-      randomFunction.number
-    }
-  const includeSymbols = confirm("Would you like Symbols (i.e. !$%&'()*+,-./:;<=>?@[^/]_`{|}~) in your password?" + "\n" + "Select Okay to include Symbols.");
-    if (includeSymbols == true) {
-      randomFunction.symbol
-    }
-
 }
-
-
-
-
-const randomFunction = {
-  capital: getRandomCapital,
-  lower: getRandomLower,
-  number: getRandomNumber,
-  symbol: getRandomSymbol,
-}
-
-
-function getRandomCapital() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function getRandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function getRandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function getRandomSymbol() {
-  const symbols = " !$%&'()*+,-./:;<=>?@[^/]_`{|}~";
-  return symbols[Math.floor(Math.random() * symbols.length)];
-}
-
-generatePassword();
-
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
